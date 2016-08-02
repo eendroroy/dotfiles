007d0c96f2f85e67fa771dc955d3bdd18f4dbd37
indrajit <eendroroy@gmail.com>
2016-08-02 11:12:30 +0600

    remove nvim.sh and zsh.sh

--- recipes/vim-plug.sh
+++ recipes/vim-plug.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "########## Installing antigen ##########"
+if [[ -d ~/.config/nvim/autoload/plug.vim ]]; then
+  echo "########## Vim-plug already installed. Skipping ..."
+else
+  curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+echo "########################################"
--- recipes/zsh.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -L /usr/bin/zsh ]; then
-	sudo apt install -y zsh
-	echo "Configuring zsh as default shell"
-	chsh -s $(which zsh)
-fi
