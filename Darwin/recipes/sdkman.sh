fa6308d559b143c0fd8d99052f714b34f415e4b3
indrajit <eendroroy@gmail.com>
2016-12-04 05:27:00 +0600

    update installation script

--- Darwin/recipes/sdkman.sh
+++ Darwin/recipes/sdkman.sh
@@ -3,5 +3,4 @@ curl -s "https://get.sdkman.io" | bash
 
 export SDKMAN_DIR="/home/indrajit/.sdkman"
 [[ -s "/home/indrajit/.sdkman/bin/sdkman-init.sh" ]] && source "/home/indrajit/.sdkman/bin/sdkman-init.sh"
-sdk selfupdate
 
