81c10fb2faaf971c8082b2c18791850e6f293da1
indrajit <eendroroy@gmail.com>
2016-12-04 06:12:48 +0600

    update hg templates installation

--- Linux/recipes/hg-color.sh
+++ Linux/recipes/hg-color.sh
@@ -5,13 +5,7 @@ echo "templatedir $templateDir"
 if [[ -d $templateDir ]]; then
   echo "Mercurial cmdline maps already exists. Skipping ..."
 else
-  os=`uname`
-  if [[ "${os}" == "Linux" ]]; then
-    sudo mkdir -p /usr/share/mercurial/templates
-    sudo sh -c 'cd /usr/share/mercurial/templates/ && git clone https://github.com/eendroroy/mercurial-cli-templates.git'
-  elif [[ "${os}" == "Darwin" ]]; then
-    sudo mkdir -p /usr/local/share/mercurial/templates
-    sudo sh -c 'cd /usr/local/share/mercurial/templates/ && git clone https://github.com/eendroroy/mercurial-cli-templates.git'
-  fi
+  sudo mkdir -p /usr/share/mercurial/templates
+  sudo sh -c 'cd /usr/share/mercurial/templates/ && git clone https://github.com/eendroroy/mercurial-cli-templates.git'
 fi
 echo "##############################################"
