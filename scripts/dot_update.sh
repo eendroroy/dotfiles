2ec1a45d46b3da6ebc313aee016e15173a2fb6c3
indrajit <eendroroy@gmail.com>
2017-01-18 22:53:41 +0600

    update dot-update

--- scripts/dot_update.sh
+++ scripts/dot_update.sh
@@ -2,7 +2,7 @@
 
 COLOR='\033[1;34m'
 NC='\033[0m'
-paths=`find ~ -maxdepth 4 -name ".git" 2>/dev/null | sed "s|.git||g" | grep "/\." | grep -v "\.antigen/repos/\|\.gvfs\|\.cache/dconf"`
+paths=`find ~ -maxdepth 4 -name ".git" 2>/dev/null | sed "s|.git||g" | grep -P '(?!.*\.antigen/repos)/\.'`
 for path in ${paths}; do
   echo -e "${COLOR}    --> ${path}${NC}"
   pushd ${path} > /dev/null
