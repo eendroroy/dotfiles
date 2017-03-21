bf71dc56b169650905d6fe73c8652e30bcd93e94
indrajit <eendroroy@gmail.com>
2017-03-22 00:59:47 +0600

    Merge branch 'master' of github.com:eendroroy/dotfiles

--- common/recipes/hg-color.sh
+++ common/recipes/hg-color.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo "############ installing hg-colors ############"
-templateDir="/usr/share/mercurial/templates/mercurial-cli-templates"
+templateDir="/usr/local/share/mercurial/templates/mercurial-cli-templates"
 echo "templatedir $templateDir"
 if [[ -d $templateDir ]]; then
   echo "Mercurial cmdline maps already exists. Skipping ..."
 else
   sudo mkdir -p /usr/local/share/mercurial/templates
   sudo sh -c 'cd /usr/local/share/mercurial/templates/ && git clone https://github.com/eendroroy/mercurial-cli-templates.git'
+fi
 echo "##############################################"
