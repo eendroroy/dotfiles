530b53b8db8df1e46ede93ea59d883f0eab8f7e9
indrajit <eendroroy@gmail.com>
2017-05-24 18:54:19 +0600

    update vim-plug installation

--- common/recipes/vim-plug.sh
+++ common/recipes/vim-plug.sh
@@ -3,6 +3,7 @@ echo "########## Installing antigen ##########"
 if [[ -d ~/.config/nvim/autoload/plug.vim ]]; then
   echo "########## Vim-plug already installed. Skipping ..."
 else
+  echo "########## Installing vim-plug ..."
   curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   which nvim > /dev/null && nvim +PlugUpgrade +PlugUpdate +PlugClean +qa
