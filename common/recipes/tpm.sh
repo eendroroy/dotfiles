637ee436ecbb844c50c1b82de543893194280865
indrajit <eendroroy@gmail.com>
2017-11-13 14:25:49 +0600

    tpm

--- common/recipes/tpm.sh
+++ common/recipes/tpm.sh
@@ -2,6 +2,7 @@ echo "########## Installing tmp##########"
 if [ -d ~/.tmux/plugins/tpm ]; then
   echo "########## tpm already installed. Skipping..."
 else
+	mkdir ~/.tmux/plugins
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 echo "################################################"
