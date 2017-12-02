0b12a4e819c5d99fec1b634fb3ef252bf1e17001
indrajit <eendroroy@gmail.com>
2017-12-03 04:22:24 +0600

    update messages

--- copy.sh
+++ copy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 if [ ! -d ${HOME}/.config/fish ]; then
-	echo "Creating ~/.config"
+	echo "\033[32m ==> Creating ~/.config \033[39m"
 	mkdir -p "${HOME}/.config/fish"
 fi
 
+echo -e "  \033[94m ==> \033[39m"
+echo -e "  \033[94m ==> \033[32mCopying fishfile \033[39m"
 cp "`pwd`/common/fish/fishfile" "${HOME}/.config/fish/fishfile"
--- link.sh
+++ link.sh
@@ -1,49 +1,44 @@
 #!/usr/bin/env bash
 
+function create_link() {
+    if [ -e $2 ]; then
+        echo -e "  \033[94m ==> \033[33m~{$2} already exists... Skipping. \033[39m"
+    else
+        echo -e "  \033[94m ==> \033[32mCreating symlink of $file \033[39m"
+        ln -s $1 $2
+    fi
+}
+
 DOTFILES="`pwd`/`uname` `pwd`/common"
 
-echo -e "\nCreating symlinks"
-echo "=============================="
+echo -e "  \033[94m ==> \033[39m"
+echo -e "  \033[94m ==> \033[32mCreating symlinks \033[39m"
 
-linkables=$( find $DOTFILES -name "*.symlink" )
+linkables=$( find ${DOTFILES} -name "*.symlink" )
 for file in $linkables; do
-    target="$HOME/.$( basename $file '.symlink')"
-    if [ -e $target ]; then
-        echo "~{$target#$HOME} already exists... Skipping."
-    else
-        echo "Creating symlink of $file"
-        ln -s $file $target
-    fi
+    create_link $file "${HOME}/.$( basename $file '.symlink')"
 done
 
-echo -e "\n\ninstalling to ~/.config"
-echo "=============================="
-if [ ! -d $HOME/.config ]; then
-	echo "Creating ~/.config"
-	mkdir -p $HOME/.config
+echo -e "  \033[94m ==> \033[39m"
+echo -e "  \033[94m ==> \033[32mInstalling to ~/.config \033[39m"
+if [ ! -d ${HOME}/.config ]; then
+	echo -e "  \033[94m ==> Creating ~/.config \033[39m"
+	mkdir -p ${HOME}/.config
 fi
 
-if [[ $( ls `pwd`/common/config -1 ) ]]; then
-    for config in `pwd`/common/config/*; do
-        target=$HOME/.config/$( basename $config )
-        if [ -e $target ]; then
-            echo "~{$target#$HOME} already exists... Skipping."
-        else
-            echo "$config ==> $target"
-            ln -s $config $target
-        fi
+if [[ -d `pwd`/common/config ]]; then
+    for config in $( ls -1 `pwd`/common/config ); do
+        create_link $config "${HOME}/.config/$( basename $config )"
     done
 fi
 
-if [[ $( ls `pwd`/`uname`/config -1 ) ]]; then
-    for config in `pwd`/`uname`/config/*; do
-        target=$HOME/.config/$( basename $config )
-        if [ -e $target ]; then
-            echo "~{$target#$HOME} already exists... Skipping."
-        else
-            echo "$config ==> $target"
-            ln -s $config $target
-        fi
+if [[ -d `pwd`/`uname`/config ]]; then
+    for config in $( ls -1 `pwd`/`uname`/config ); do
+        create_link $config "${HOME}/.config/$( basename $config )"
     done
+fi
+
+if [[ "`uname`" == "Darwin" ]]; then
+    create_link "`pwd`/`uname`/Brewfile" "${HOME}/Brewfile"
 fi
 
