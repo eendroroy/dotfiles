a544ee29e349f49e9b1728a794cc689021799750
indrajit <eendroroy@gmail.com>
2025-09-14 10:56:13 +0600

    update asdf

--- bootstrap-asdf.sh
+++ bootstrap-asdf.sh
@@ -1,22 +1,4 @@
 #!/usr/bin/env bash
 
-SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-source "${SCRIPT_LOCATION}/util/style.sh"
-
-PLUGINS=(nodejs python elixir golang ruby)
-
-for plugin in "${PLUGINS[@]}"
-do
-  echo -e "$(printPrimaryMessage "adding ${plugin} plugin")"
-  asdf plugin add "${plugin}"
-done
-
-for plugin in "${PLUGINS[@]}"
-do
-  echo -e "$(printPrimaryMessage "installing latest ${plugin}")"
-  asdf install "${plugin}" latest
-
-  echo -e "$(printPrimaryMessage "setting ${plugin}:latest as global")"
-  asdf global "${plugin}" latest
-done
+mkdir -p "${ASDF_DATA_DIR:-$HOME/.asdf}/completions"
+asdf completion zsh > "${ASDF_DATA_DIR:-$HOME/.asdf}/completions/_asdf"
