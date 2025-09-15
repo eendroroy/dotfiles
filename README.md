03103da1a3718ae39b3fd28a3354e178bc6e7099
indrajit <eendroroy@gmail.com>
2025-09-15 20:14:15 +0600

    update installer and uninstaller

--- README.md
+++ README.md
@@ -7,7 +7,17 @@ Clone the repo:
 ```bash
 git clone https://github.com/eendroroy/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
-bash install
+bash dot_install 
+bash dot_install --force # force install even if already installed from different source
+```
+
+## Uninstallation
+
+```bash
+cd ~/.dotfiles
+bash dot_uninstall                  # uninstall from cache
+bash dot_uninstall --search         # uninstall by links
+bash dot_uninstall --search --force # uninstall by links and remove even if installed from different source
 ```
 
 ## License
--- scripts/link.sh
+++ scripts/link.sh
@@ -2,40 +2,91 @@
 
 touch "${HOME}/.dotfiles_uninstall.txt"
 
-source "${SCRIPT_LOCATION}/scripts/style.sh"
-
 function create_link() {
-    if [[ -e "${2}" ]]; then
-        echo -e "$(printWarningMessage "[${1} -> ${2}] already exists... Skipping.")"
+    local __link
+    __link=$(readlink "${2}")
+    if [[ "${3}" != "true" && -e "${2}" ]]; then
+        echo -e "$(printWarningMessage "Already exists. Skipping...")"
+        echo -e "$(printWarningMessageC "  Source: '${1}'")"
+        echo -e "$(printWarningMessageC "  Destination: '${2}']")"
+    elif [[ "${3}" = "true" && -e "${2}" && "${__link}" != "${1}" ]]; then
+        echo -e "$(printNoticedMessage "Already exists. Replacing...")"
+        echo -e "$(printNoticedMessageC "  Source:          '${__link}'")"
+        echo -e "$(printNoticedMessageC "  Expected Source: '${1}'")"
+        echo -e "$(printNoticedMessageC "  Destination: '${2}']")"
+        rm "${2}"
+        echo "${2}" >> "${HOME}/.dotfiles_uninstall.txt"
+        ln -s "${1}" "${2}"
+    elif [[ "${3}" = "true" && -e "${2}" && "${__link}" = "${1}" ]]; then
+        echo -e "$(printWarningMessage "Already exists. Skipping...")"
+        echo -e "$(printWarningMessageC "  Source: '${1}'")"
+        echo -e "$(printWarningMessageC "  Destination: '${2}']")"
     else
-        echo -e "$(printSecondaryMessage "[${1} -> ${2}]")"
+        echo -e "$(printSecondaryMessage "['${1}' -> '${2}']")"
         echo "${2}" >> "${HOME}/.dotfiles_uninstall.txt"
         ln -s "${1}" "${2}"
     fi
 }
 
-for prefix in "common" "$(uname)"; do
-    echo -e "$(printPrimaryMessage "Installing from ${prefix} to ~/")"
-    while IFS= read -r file; do
-        create_link "${file}" "${HOME}/.$(basename "${file}" '.symlink')"
-    done < <(find "${SCRIPT_LOCATION}/${prefix}" -name "*.symlink")
-done
+function remove_link() {
+    local __link
+    __link=$(readlink "${2}")
+    if [[ ! -e ${__link} ]]; then
+        echo -e "$(printWarningMessage "Does not exist. Skipping...")"
+        echo -e "$(printWarningMessageC "  Source: '${1}'")"
+        echo -e "$(printWarningMessageC "  Destination: '${2}']")"
+    elif [[ "${3}" != "true" && ${__link} != "${1}" ]]; then
+        echo -e "$(printWarningMessage "Exists from different source. Skipping...")"
+        echo -e "$(printWarningMessageC "  Source:          '${__link}'")"
+        echo -e "$(printWarningMessageC "  Expected Source: '${1}'")"
+        echo -e "$(printWarningMessageC "  Destination: '${2}']")"
+    elif [[ "${3}" = "true" && ${__link} != "${1}" ]]; then
+        echo -e "$(printNoticedMessage "Exists from different source. Replacing...")"
+        echo -e "$(printNoticedMessageC "  Source:          '${__link}'")"
+        echo -e "$(printNoticedMessageC "  Expected Source: '${1}'")"
+        echo -e "$(printNoticedMessageC "  Destination: '${2}']")"
+        rm "${2}"
+    else
+        echo -e "$(printSecondaryMessage "Removing ['${__link}' -> '${2}'] ...")"
+        rm "${2}"
+    fi
+}
 
-for prefix in "common" "$(uname)"; do
-    for dir in ".config" "bin"; do
-        if [[ -d ${SCRIPT_LOCATION}/${prefix}/${dir} ]]; then
-          echo -e "$(printPrimaryMessage "Installing from ${prefix} to ~/${dir}")"
-          if [[ ! -d ${HOME}/${dir} ]]; then
-              echo -e "$(printPrimaryMessage "Creating ~/${dir}")"
-              mkdir -p "${HOME}/${dir}"
-          fi
-          for linkable in "${SCRIPT_LOCATION}/${prefix}/${dir}"/*; do
-              create_link "${linkable}" "${HOME}/${dir}/$( basename "${linkable}" )"
-          done
-      fi
+function __handle_links() {
+    local __command;
+    __command=$(
+        case $1 in
+            install)
+                echo -n 'create_link'
+                ;;
+            uninstall)
+                echo -n 'remove_link'
+                ;;
+        esac
+    )
+    for prefix in "common" "$(uname)"; do
+        echo -e "$(printPrimaryMessage "Installing from ${prefix} to ~/")"
+        while IFS= read -r file; do
+            ${__command} "${file}" "${HOME}/.$(basename "${file}" '.symlink')" ${2}
+        done < <(find "${SCRIPT_LOCATION}/${prefix}" -name "*.symlink")
     done
-done
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    create_link "${SCRIPT_LOCATION}/$(uname)/Brewfile" "${HOME}/Brewfile"
-fi
+    for prefix in "common" "$(uname)"; do
+        for dir in ".config" "bin"; do
+            if [[ -d ${SCRIPT_LOCATION}/${prefix}/${dir} ]]; then
+              echo -e "$(printPrimaryMessage "Installing from ${prefix} to ~/${dir}")"
+              if [[ ! -d ${HOME}/${dir} ]]; then
+                  echo -e "$(printPrimaryMessage "Creating ~/${dir}")"
+                  mkdir -p "${HOME}/${dir}"
+              fi
+              for linkable in "${SCRIPT_LOCATION}/${prefix}/${dir}"/*; do
+                  ${__command} "${linkable}" "${HOME}/${dir}/$( basename "${linkable}" )" ${2}
+              done
+          fi
+        done
+    done
+
+    if [[ "$(uname)" == "Darwin" ]]; then
+        ${__command} "${SCRIPT_LOCATION}/$(uname)/Brewfile" "${HOME}/Brewfile" ${2}
+    fi
+}
