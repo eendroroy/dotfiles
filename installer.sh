76ffe581536d7c5eea4cd0db3e5d3b2ae00d5a7c
indrajit <eendroroy@gmail.com>
2025-09-21 11:20:10 +0600

    upate installer

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ -d ~/.dotfiles ]] && echo "Already installed at '~/.dotfiles'" && exit 1
+[[ -d ~/.dotfiles ]] && echo "Already installed at '~/.dotfiles'" && exit 1
 git clone https://github.com/eendroroy/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles || exit 1
 bash dot_install || exit 1
