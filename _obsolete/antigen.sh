c89a048c5211a7e175477112e1bad9c37860338e
indrajit <eendroroy@gmail.com>
2025-09-22 16:21:11 +0600

    refactor

--- _obsolete/antigen.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source "${SCRIPT_LOCATION}/scripts/installer_template.sh"
-
-executeInstallWithTargetCheck antigen .antigen "
-git clone https://github.com/zsh-users/antigen.git ${HOME}/.antigen --depth=1
-"
