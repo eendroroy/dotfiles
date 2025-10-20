3397a616fcf4d5de010c2025cc1f194cad0e7886
indrajit <eendroroy@gmail.com>
2025-10-20 16:59:20 +0600

    update styles

--- __functions/__style.sh
+++ __functions/__style.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
-__m_primary()     { echo -e " \e[38;5;5m ─\e[m \e[38;5;2m${1}\e[m"; }
-__m_primary_c()   { echo -e " \e[38;5;5m  \e[m \e[38;5;2m${1}\e[m"; }
-__m_secondary()   { echo -e " \e[38;5;5m ─\e[m \e[38;5;4m${1}\e[m"; }
-__m_secondary_c() { echo -e " \e[38;5;5m  \e[m \e[38;5;4m${1}\e[m"; }
-__m_notice()      { echo -e " \e[38;5;5m ─\e[m \e[38;5;1m(!)\e[m \e[38;5;4m${1}\e[m"; }
-__m_notice_c()    { echo -e " \e[38;5;5m  \e[m \e[38;5;1m(!)\e[m \e[38;5;4m${1}\e[m"; }
-__m_warning()     { echo -e " \e[38;5;5m ─\e[m \e[38;5;3m${1}\e[m"; }
-__m_warning_c()   { echo -e " \e[38;5;5m  \e[m \e[38;5;3m${1}\e[m"; }
-__m_error()       { echo -e " \e[38;5;5m ─\e[m \e[38;5;1m${1}\e[m"; }
-__m_error_c()     { echo -e " \e[38;5;5m  \e[m \e[38;5;1m${1}\e[m"; }
+__m_primary()     { echo -e " $(dim)$(fg_bright_black)  $(reset_all)$(fg_green)$(bold)${1}$(reset_all)"; }
+__m_primary_c()   { echo -e " $(dim)$(fg_bright_black)   $(reset_all)$(fg_green)$(bold)${1}$(reset_all)"; }
+__m_secondary()   { echo -e " $(dim)$(fg_bright_black) 󰇘 $(reset_all)$(fg_cyan)$(dim)${1}$(reset_all)"; }
+__m_secondary_c() { echo -e " $(dim)$(fg_bright_black)   $(reset_all)$(fg_cyan)$(dim)${1}$(reset_all)"; }
+__m_warning()     { echo -e " $(dim)$(fg_bright_black) 󰇘 $(reset_all)$(fg_yellow)$(dim)$(underline)${1}$(reset_all)"; }
+__m_warning_c()   { echo -e " $(dim)$(fg_bright_black)   $(reset_all)$(fg_yellow)$(dim)$(underline)${1}$(reset_all)"; }
+__m_error()       { echo -e " $(dim)$(fg_bright_black) 󰇘 $(reset_all)$(fg_bright_red)$(dim)$(underline)${1}$(reset_all)"; }
+__m_error_c()     { echo -e " $(dim)$(fg_bright_black)   $(reset_all)$(fg_bright_red)$(dim)$(underline)${1}$(reset_all)"; }
 
 export -f __m_primary
 export -f __m_primary_c
 export -f __m_secondary
 export -f __m_secondary_c
-export -f __m_notice
-export -f __m_notice_c
 export -f __m_warning
 export -f __m_warning_c
 export -f __m_error
