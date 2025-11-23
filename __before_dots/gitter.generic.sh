5d9539eaa27a7813b72cf8434250b55dbbd570ce
indrajit <eendroroy@gmail.com>
2025-11-23 13:53:23 +0600

    add gitter installation

---
+++ __before_dots/gitter.generic.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Copyright (C) Indrajit Roy <eendroroy@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+__install_from_git eendroroy/gitter .gitter
